10f75a3dfc9ad67985fc7ac741eb99f21625d8fc
Pandit <dhamdherepandit@gmail.com>
2022-10-02 21:44:15 +0530

    modidied

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -335,7 +335,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Your Name</footer>
+      <footer className={styles.footer}>Made with &#10084; by Pandit</footer>
     </div>
   );
 }
